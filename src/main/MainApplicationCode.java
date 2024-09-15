08c3ae1cddf7b43491708a4f75b729e1a4c4a3e6
kadukrunal <krunalkadu.01@gmail.com>
2024-09-15 13:37:03 +0530

    Updated Main Application Code

---
+++ src/main/MainApplicationCode.java
@@ -0,0 +1,10 @@
+package main;
+
+public class MainApplicationCode {
+	
+	public static void main(String[] args) {
+		System.out.println("This is an online shopping website");
+		System.out.println("This is a Demo Main Application for Learning Git and JEnkins Integration");
+	}
+
+}
